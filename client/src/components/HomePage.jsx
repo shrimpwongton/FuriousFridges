f5d023594e09f1c538addcb068b8f082269c8328
Anthony Wong <shrimpwongton@users.noreply.github.com>
2017-05-29 14:19:00 -0700

    (feat) Kept photos locally

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -17,25 +17,29 @@ class HomePage extends React.Component {
   }
   render () {
     const images = [
-      'https://s-media-cache-ak0.pinimg.com/originals/83/8f/06/838f06aa06f0ca04db45ce3bf73516f8.jpg',
-      'http://magic.wizards.com/sites/mtg/files/images/featured/GP_HongKong.jpg',
-      'http://japanphototrip.com/wp-content/uploads/2015/02/fushimi-inari-daisha.jpg',
-      'http://www.dyspraxia.ie/userfiles/images/Singapore-1540.jpg',
-      'https://media.timeout.com/images/100432575/image.jpg',
-      'http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1444253482/DG2015-san-francisco.jpg?itok=MdRJm2Zo',
-      'https://www.mostbeautifulspots.com/wp-content/uploads/2016/10/Zhangjiajie-National-Forest-Park-China.jpg',
-      'http://lowerantelope.com/wp-content/uploads/2016/05/LA-gallery-img1_0006_Layer-4-1024x768.jpg',
-      'https://upload.wikimedia.org/wikipedia/commons/b/ba/Sagrada_Familia_nave_roof_detail.jpg',
-      'http://images.huffingtonpost.com/2015-11-19-1447968585-1661590-6672156239_89c77d53d8_o.jpg',
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Taj_Mahal_2012.jpg',
-      'https://www.outdoorproject.com/sites/default/files/styles/cboxshow/public/img_2713.jpg?itok=eEZBZMLX',
-      'http://static.thousandwonders.net/Jiufen.original.5446.jpg',
-      'http://www.myhockeytournaments.com/img/sites/chicago/001.jpg',
-
+      '/assets/LibertySquare.jpg',
+      '/assets/VictoriaHarbor.jpg',
+      '/assets/Kyoto.jpg',
+      '/assets/Singapore.jpg',
+      '/assets/CentralPark.jpg',
+      '/assets/GoldenGate.jpg',
+      '/assets/Zhangjiajie.jpg',
+      '/assets/AntelopeValley.jpg',
+      '/assets/SangradaFamilia.jpg',
+      '/assets/Paris.jpg',
+      '/assets/TajMahal.jpg',
+      '/assets//HalfDome.jpg',
+      '/assets/JiuFen.jpg',
+      '/assets/Chicago.jpg',
+      '/assets/Uluru.jpg',
+      '/assets/Phuket.jpg',
+      '/assets/GuangHuaMen.jpg',
+      '/assets/BrandenburgGate.jpg',
+      '/assets/StBasilsCathedral.jpg',
     ];
     const captions = [
-      'Liberty Square, Taipei · 自由廣場，臺北',
-      'Victoria Peak, Hong Kong · 太平山，香港',
+      'Liberty Square, Taipei · 自由廣場，臺北市',
+      'Victoria Harbor, Hong Kong · 維多利亞港，香港',
       'Fushimi Inari Taisha, Kyoto · 伏見稲荷大社，京都',
       'Gardens By The Bay, Singapore · 滨海湾花园资讯指南，新加坡',
       'Central Park, New York City',
@@ -47,7 +51,12 @@ class HomePage extends React.Component {
       'Taj Mahal, India',
       'Half Dome, California',
       'Jiufen, Taiwan · 九分，臺灣',
-      'Millennium Park, Chicago'
+      'Millennium Park, Chicago',
+      'Uluru, Australia',
+      'Phuket, Thailand · ภูเก็ต，ประเทศไทย',
+      'Gwanghwamun, South Korea · 광화문，대한민국',
+      'Brandenburg Gate, Berlin · Brandenburger Tor, Berlin',
+      'St. Basil\'s Cathedral, Moscow · Собор Василия Блаженного, Москва́'
     ];
     const randomNumber = Math.floor(Math.random()*images.length);
 
@@ -75,17 +84,17 @@ class HomePage extends React.Component {
       subHeaderStyle: {
         fontFamily: "'Roboto', sans-serif",
         color: grey300,
-        fontSize: '36px',
+        fontSize: '3em',
       },
       headerStyle: {
-        fontFamily: "'Roboto', sans-serif",
+        fontFamily: "'Roboto-Light', sans-serif",
         color: 'white',
-        fontSize: '45px',
+        fontSize: '8em',
       },
       captionStyle: {
         fontFamily: "'Roboto', sans-serif",
         color: 'white',
-        fontSize: '16px',
+        fontSize: '1em',
         margin: 0,
         top: 'auto',
         right: 'auto',
