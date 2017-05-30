39f9e204383e0974b014933f8d6733ad042c4b0a
Anthony Wong <shrimpwongton@users.noreply.github.com>
2017-05-29 21:27:37 -0700

    (feat) Profile page updates

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {
-  blueGrey500, white, amber500
+  blueGrey500, white, amber500, grey300
 } from 'material-ui/styles/colors';
 import {
   Link,
@@ -12,6 +12,8 @@ import ActionDashboard from 'material-ui/svg-icons/action/dashboard';
 import SocialLocationCity from 'material-ui/svg-icons/social/location-city';
 import ActionList from 'material-ui/svg-icons/action/list';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -65,17 +67,34 @@ class Profile extends React.Component {
                 />
               </Link>
             </ToolbarGroup>
+            <ToolbarGroup style={styles.signInStyle}>
+              <IconButton
+                tooltip="Settings">
+                <ActionSettings
+                  color = {white}
+                />
+              </IconButton>
+              <a href='/logout'>
+                <FlatButton
+                  style={styles.whiteTextStyle}
+                  label="LOG OUT"
+                />
+              </a>
+            </ToolbarGroup>
           </Toolbar>
         </MuiThemeProvider>
         <MuiThemeProvider>
           <Tabs
-            inkBarStyle={{background: amber500, zIndex: 500}}>
+            tabItemContainerStyle={{width: '70%'}}
+            inkBarStyle={{background: amber500, zIndex: 500}}
+            contentContainerStyle={{background: grey300}}
+            style={{background: blueGrey500}}>
             <Tab
               icon={<ActionDashboard/>}
               label="DASHBOARD"
               style={styles.tabStyle}>
               <div>
-                stuff here
+                Dashboard React Component Here
               </div>
             </Tab>
             <Tab
@@ -84,24 +103,16 @@ class Profile extends React.Component {
               style={styles.tabStyle}
             >
               <div>
-                stuff here
+                City React Component Here
               </div>
             </Tab>
             <Tab
               icon={<ActionList/>}
-              label="Question Board"
+              label="QUESTIONS"
               style={styles.tabStyle}
             >
               <div>
-                stuff here
-              </div>
-            </Tab>
-            <Tab
-              icon={<ActionSettings/>}
-              label="Settings"
-              style={styles.tabStyle}>
-              <div>
-                stuff here
+                Question Board Component Here
               </div>
             </Tab>
           </Tabs>
