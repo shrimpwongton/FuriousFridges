1b45bb8cf3e3731a779d65f86d875e426fc3b49d
Kara Marnell <kmarnell@gmail.com>
2017-06-01 15:41:04 -0700

    slight edits before rebase

--- client/src/components/CityInfo.jsx
+++ client/src/components/CityInfo.jsx
@@ -17,9 +17,6 @@ import SocialGroup from 'material-ui/svg-icons/social/group';
 import {
   amber500, green500, lightGreen500, orange500, red500,
 } from 'material-ui/styles/colors';
-var Highcharts = require('highcharts');
-
-require('highcharts/modules/funnel')(Highcharts);
 
 
 class CityInfo extends React.Component {
