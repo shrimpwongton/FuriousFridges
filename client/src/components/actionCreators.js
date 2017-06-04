c0ed191b56fafc6c8e5369a7232c965a2c9d7f8b
Salih <salih7@gmail.com>
2017-06-03 18:04:47 -0700

    Modify actionCreators file to satify linting requirements for function definition

--- client/src/components/actionCreators.js
+++ client/src/components/actionCreators.js
@@ -8,38 +8,38 @@ import { SET_QUESTION,
          SET_CURRENT_VIEW,
          SET_CURRENT_USER } from './actions';
 
-export function setQuestion(question) {
+export const setQuestion = (question) => {
   return { type: SET_QUESTION, payload: question };
-}
+};
 
-export function setAnswer(answer) {
+export const setAnswer = (answer) => {
   return { type: SET_ANSWER, payload: answer };
-}
+};
 
-export function setQuestions(questions) {
+export const setQuestions = (questions) => {
   return { type: SET_QUESTIONS, payload: questions };
-}
+};
 
-export function setAnswers(answers) {
+export const setAnswers = (answers) => {
   return { type: SET_ANSWERS, payload: answers };
-}
+};
 
-export function setQuestionDialog(questionDialog) {
+export const setQuestionDialog = (questionDialog) => {
   return { type: SET_QUESTION_DIALOG, payload: questionDialog };
-}
+};
 
-export function setErrorText(errorText) {
+export const setErrorText = (errorText) => {
   return { type: SET_ERROR_TEXT, payload: errorText };
-}
+};
 
-export function setCurrentQuestion(currentQuestion) {
+export const setCurrentQuestion = (currentQuestion) => {
   return { type: SET_CURRENT_QUESTION, payload: currentQuestion };
-}
+};
 
-export function setCurrentView(currentView) {
+export const setCurrentView = (currentView) => {
   return { type: SET_CURRENT_VIEW, payload: currentView };
-}
+};
 
-export function setCurrentUser(currentUser) {
+export const setCurrentUser = (currentUser) => {
   return { type: SET_CURRENT_USER, payload: currentUser };
-}
+};
