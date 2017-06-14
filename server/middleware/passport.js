28393da54ee1b6f17b05045204c59441e2b7fe46
Salih <salih7@gmail.com>
2017-06-14 11:34:59 -0700

    Modify Google oauth config variables

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -112,31 +112,31 @@ passport.use('local-login', new LocalStrategy({
   }));
 
 passport.use('google', new GoogleStrategy({
-  clientID: config.Google.clientID,
-  clientSecret: config.Google.clientSecret,
-  callbackURL: config.Google.callbackURL
+  clientID: process.env.GOOGLE_CLIENT_ID || config.Google.clientID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET || config.Google.clientSecret,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || config.Google.callbackURL
 },
   (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('google', profile, done))
 );
 
-passport.use('facebook', new FacebookStrategy({
-  clientID: config.Facebook.clientID,
-  clientSecret: config.Facebook.clientSecret,
-  callbackURL: config.Facebook.callbackURL,
-  profileFields: ['id', 'emails', 'name']
-},
-  (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('facebook', profile, done))
-);
+// passport.use('facebook', new FacebookStrategy({
+//   clientID: config.Facebook.clientID,
+//   clientSecret: config.Facebook.clientSecret,
+//   callbackURL: config.Facebook.callbackURL,
+//   profileFields: ['id', 'emails', 'name']
+// },
+//   (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('facebook', profile, done))
+// );
 
-//REQUIRES PERMISSIONS FROM TWITTER TO OBTAIN USER EMAIL ADDRESSES
-passport.use('twitter', new TwitterStrategy({
-  consumerKey: config.Twitter.consumerKey,
-  consumerSecret: config.Twitter.consumerSecret,
-  callbackURL: config.Twitter.callbackURL,
-  userProfileURL: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true'
-},
-  (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('twitter', profile, done))
-);
+// //REQUIRES PERMISSIONS FROM TWITTER TO OBTAIN USER EMAIL ADDRESSES
+// passport.use('twitter', new TwitterStrategy({
+//   consumerKey: config.Twitter.consumerKey,
+//   consumerSecret: config.Twitter.consumerSecret,
+//   callbackURL: config.Twitter.callbackURL,
+//   userProfileURL: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true'
+// },
+//   (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('twitter', profile, done))
+// );
 
 const getOrCreateOAuthProfile = (type, oauthProfile, done) => {
   return models.Auth.where({ type, oauth_id: oauthProfile.id }).fetch({
