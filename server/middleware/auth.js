74aa179b5e16f5456a7529326820aba1a97bbb1e
Salih <salih7@gmail.com>
2017-06-14 17:14:46 -0700

    Add REDIS_URL to createClient as an argument

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
+const redisClient = require('redis').createClient(process.env.REDIS_URL) || require('redis').createClient;
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
